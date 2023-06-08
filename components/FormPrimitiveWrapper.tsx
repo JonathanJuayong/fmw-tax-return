be8b53887ad65eed9739135321b877d24a59db2d
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-06-08 12:11:34 +1000

    feat: extend interface to have more control on wrapper div that wraps the whole form primitive

--- components/FormPrimitiveWrapper.tsx
+++ components/FormPrimitiveWrapper.tsx
@@ -1,19 +1,26 @@
-import {ReactNode} from "react";
+import {HTMLAttributes, ReactNode} from "react";
 import {FormControl, FormItem, FormLabel, FormMessage} from "@/components/ui/Form";
 import Inline from "@/components/layout/Inline";
 import {HelpCircle} from "lucide-react";
 import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
 
-interface FormPrimitiveWrapperProps {
+interface FormPrimitiveWrapperProps extends HTMLAttributes<HTMLDivElement>{
   children: ReactNode
   label: string
   description: string
 }
 
-export default function FormPrimitiveWrapper({children, label, description}: FormPrimitiveWrapperProps) {
+export default function FormPrimitiveWrapper(
+  {
+    children,
+    label,
+    description,
+    ...props
+  }: FormPrimitiveWrapperProps
+) {
   return (
-    <FormItem>
-      <Inline justifyContent="start" gap="xxs" className="mb-4">
+    <FormItem {...props}>
+      <Inline justifyContent="start" gap="xxs" className="mb-2">
         <FormLabel>{label}</FormLabel>
         <Popover>
           <PopoverTrigger><HelpCircle className="h-4 w-4"/></PopoverTrigger>
