34520bb004ac9aeb6f2199af45d9db341c474039
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-06-08 15:40:49 +1000

    feat: create additional schema for specific form questions

--- utils/formSchema.ts
+++ utils/formSchema.ts
@@ -16,6 +16,50 @@ export const personalInfoSchema = z.object({
   })
 })
 
+const bankInterestSchema = z.object({
+  bankName: z.string(),
+  bsb: z.string(),
+  accountNumber: z.string(),
+  interestAmount: z.number(),
+  isJointAccount: z.boolean()
+})
+
+const dividendsSchema = z.object({
+  companyName: z.string(),
+  srcOrHrn: z.string(),
+  datePaid: z.string(),
+  unfrankedAmount: z.number(),
+  frankedAmount: z.number(),
+  imputationCredit: z.string()
+})
+
+const rentalPropertySchema = z.object({
+  address: z.string(),
+  postcode: z.string(),
+  cost: z.number(),
+  dateOfPurchase: z.string(),
+  firstDateOfRent: z.string(),
+  yearBuilt: z.string(),
+  percentageOwned: z.number(),
+  isRegisteredForLandTax: z.boolean(),
+  outstandingLoanAmount: z.number(),
+  estimatedMarketValue: z.number()
+})
+
+export const incomeSchema = z.object({
+  bankInterest: z.array(bankInterestSchema),
+  dividends: z.array(dividendsSchema),
+  rentalProperty: z.array(rentalPropertySchema),
+})
+
+export const deductionsSchema = z.object({
+  motorVehicle: z.object({}),
+  workRelatedTravel: z.object({}),
+  otherDeductions: z.object({}),
+})
+
 export const mainSchema = z.object({
-  personalInfo: personalInfoSchema
+  personalInfo: personalInfoSchema,
+  income: incomeSchema,
+  deductions: deductionsSchema
 })
