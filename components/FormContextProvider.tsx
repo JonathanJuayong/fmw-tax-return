0941ea1e7d48047f27de9d9c33346cc0dddf8e1c
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-06-09 14:34:01 +1000

    feat: set up formState and formState setter that will be used to update the main form state within the form components

--- components/FormContextProvider.tsx
+++ components/FormContextProvider.tsx
@@ -1,9 +1,102 @@
 import {createContext, ReactElement, useContext, useState} from "react";
 import useComponentTransition from "@/utils/hooks/useComponentTransition";
+import {z} from "zod";
+import {mainSchema} from "@/utils/formSchema";
+
+const defaultFormState: z.infer<typeof mainSchema> = {
+  personalInfo: {
+    title: "",
+    lastName: "",
+    firstName: "",
+    middleName: "",
+    email: "",
+    mobile: "",
+    address: "",
+    dateOfBirth: "",
+    taxFileNumber: "",
+    bankDetails: {
+      bsb: "",
+      accountNumber: ""
+    }
+  },
+  income: {
+    bankInterest: [
+      {
+        bsb: "",
+        bankName: "",
+        interestAmount: 0,
+        accountNumber: "",
+        isJointAccount: false
+      }
+    ],
+    rentalProperty: [
+      {
+        address: "",
+        postcode: "",
+        cost: 0,
+        dateOfPurchase: "",
+        firstDateOfRent: "",
+        yearBuilt: 1900,
+        percentageOwned: 0,
+        isRegisteredForLandTax: false,
+        outstandingLoanAmount: 0,
+        estimatedMarketValue: 0
+      }
+    ],
+    dividends: [
+      {
+        companyName: "",
+        datePaid: "",
+        frankedAmount: 0,
+        unfrankedAmount: 0,
+        srnOrHin: "",
+        imputationCredit: 0
+      }
+    ]
+  },
+  deductions: {
+    motorVehicle: {
+      make: "",
+      model: "",
+      registrationNumber: "",
+      dateOfPurchase: "",
+      costOfCar: 0,
+      tradeAmount: 0,
+      reasonForClaim: "",
+      withLogbook: false,
+      businessUsePercent: 0,
+      fuelExpense: 0,
+      repairAndMaintenanceExpense: 0,
+      registrationExpense: 0,
+      insuranceExpense: 0,
+      leaseExpense: 0,
+      carWashExpense: 0,
+      interestExpense: 0,
+      kmTravelled: 0,
+    },
+    workRelatedTravel: {
+      reasonForClaim: "",
+      taxiExpense: 0,
+      tollExpense: 0,
+      parkingExpense: 0,
+      otherExpenses: []
+    },
+  }
+}
+
+const defaultMainFormState = {
+  data: defaultFormState,
+  forms: {
+    income: [""],
+    deductions: [""]
+  }
+}
+
+type MainFormState = typeof defaultMainFormState
 
 type FormContextType = {
-  formState?: any,
-  formStateSetter?: () => void,
+  formState?: MainFormState,
+  formStateUpdateHandler?: (stateSetter: (formState: MainFormState) => MainFormState) => void,
   showNextElement?: () => void,
   showPreviousElement?: () => void,
   jumpTo?: (index: number) => void
@@ -14,7 +107,7 @@ export const FormContext = createContext<FormContextType>({})
 export function useFormContext() {
   const {
     formState,
-    formStateSetter,
+    formStateUpdateHandler,
     showPreviousElement,
     showNextElement,
     jumpTo
@@ -22,7 +115,7 @@ export function useFormContext() {
 
   return {
     formState: formState!,
-    formStateSetter: formStateSetter!,
+    formStateSetter: formStateUpdateHandler!,
     showPreviousElement: showPreviousElement!,
     showNextElement: showNextElement!,
     jumpTo: jumpTo!
@@ -34,10 +127,10 @@ interface FormContextProviderProps {
 }
 
 export function FormContextProvider({components}: FormContextProviderProps) {
-  const [formState, setFormState] = useState({});
+  const [formState, setFormState] = useState<MainFormState>(defaultMainFormState);
 
-  const formStateSetter = () => {
-
+  const formStateUpdateHandler = (stateSetter: (formState: MainFormState) => MainFormState) => {
+    setFormState(prev => stateSetter(prev))
   }
 
   const {
@@ -50,7 +143,7 @@ export function FormContextProvider({components}: FormContextProviderProps) {
   return (
     <FormContext.Provider value={{
       formState,
-      formStateSetter,
+      formStateUpdateHandler: formStateUpdateHandler,
       showPreviousElement,
       showNextElement,
       jumpTo
